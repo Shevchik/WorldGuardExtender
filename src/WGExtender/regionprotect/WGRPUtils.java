e96e9061c12637823f6fb45f48ca8c96c177fb54
Shevchik <Shev4ik.den@gmail.com>
2013-08-24 22:38:22 +0400

    Safeguard against disabling regions in some world

--- src/WGExtender/regionprotect/WGRPUtils.java
+++ src/WGExtender/regionprotect/WGRPUtils.java
@@ -31,18 +31,26 @@ public class WGRPUtils {
 
 	protected static boolean isInWGRegion(WorldGuardPlugin wg, Block b)
 	{
-		if (wg.getRegionManager(b.getWorld()).getApplicableRegions(b.getLocation()).size() > 0) {return true;}
+		try {
+			if (wg.getRegionManager(b.getWorld()).getApplicableRegions(b.getLocation()).size() > 0) {return true;}
+		} catch (Exception e) {
+			//if we caught an exception here it means that regions for world are disabled
+		}
 		return false;
 	}
 	
 	protected static boolean isInTheSameRegion(WorldGuardPlugin wg, Block b1, Block b2)
 	{
-		//plain equals doesn't want to work here :(
-		List<String> ari1 = wg.getRegionManager(b1.getWorld()).getApplicableRegionsIDs(BukkitUtil.toVector(b1.getLocation()));
-		List<String> ari2 = wg.getRegionManager(b2.getWorld()).getApplicableRegionsIDs(BukkitUtil.toVector(b2.getLocation()));
-		if (ari1.equals(ari2))
-		{
-			return true;
+		try {
+			//plain equals doesn't want to work here :(
+			List<String> ari1 = wg.getRegionManager(b1.getWorld()).getApplicableRegionsIDs(BukkitUtil.toVector(b1.getLocation()));
+			List<String> ari2 = wg.getRegionManager(b2.getWorld()).getApplicableRegionsIDs(BukkitUtil.toVector(b2.getLocation()));
+			if (ari1.equals(ari2))
+			{
+				return true;
+			}
+		} catch (Exception e) {
+			//if we caught an exception here it means that regions for world are disabled
 		}
 		return false;
 	}
@@ -50,10 +58,14 @@ public class WGRPUtils {
 	
 	protected static boolean isOwnerOrMember(WorldGuardPlugin wg, Player p, Block b)
 	{
-		ApplicableRegionSet ars = wg.getRegionManager(b.getWorld()).getApplicableRegions(b.getLocation());
-		if (ars.isOwnerOfAll(wg.wrapPlayer(p)) || ars.isMemberOfAll(wg.wrapPlayer(p))) 
-		{
-			return true;
+		try {
+			ApplicableRegionSet ars = wg.getRegionManager(b.getWorld()).getApplicableRegions(b.getLocation());
+			if (ars.isOwnerOfAll(wg.wrapPlayer(p)) || ars.isMemberOfAll(wg.wrapPlayer(p))) 
+			{
+				return true;
+			}
+		} catch (Exception e) {
+			//if we caught an exception here it means that regions for world are disabled
 		}
 		return false;
 	}
