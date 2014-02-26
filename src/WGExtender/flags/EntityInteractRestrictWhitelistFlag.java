4b0bf85fdfde7e135d9c0b2c6ad3dc871265f598
Shevchik <Shev4ik.den@gmail.com>
2014-02-26 13:29:53 +0400

    Add entity interact flag classes

---
+++ src/WGExtender/flags/EntityInteractRestrictWhitelistFlag.java
@@ -0,0 +1,62 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package WGExtender.flags;
+
+import org.bukkit.command.CommandSender;
+
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.flags.Flag;
+import com.sk89q.worldguard.protection.flags.InvalidFlagFormat;
+
+public class EntityInteractRestrictWhitelistFlag extends Flag<String> {
+
+	public static EntityInteractRestrictWhitelistFlag instance = null;
+
+	public static void injectFlag() {
+		instance = new EntityInteractRestrictWhitelistFlag();
+		FlagInjector.injectFlag(instance);
+	}
+
+	public static void uninjectFlag() {
+		FlagInjector.uninjectFlag(instance);
+	}
+
+	public EntityInteractRestrictWhitelistFlag() {
+		super("entity-interact-whitelist");
+	}
+
+	@Override
+	public Object marshal(String set) {
+		return set;
+	}
+
+	@Override
+	public String parseInput(WorldGuardPlugin wg, CommandSender sender, String args) throws InvalidFlagFormat {
+		return args;
+	}
+
+	@Override
+	public String unmarshal(Object obj) {
+		if (obj instanceof String) {
+			return (String) obj;
+		} else {
+			return null;
+		}
+	}
+
+}
