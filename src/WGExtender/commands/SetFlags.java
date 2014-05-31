5cbe12d1bd403c8e33bd9cf3dbdfd480d82bb42d
Shevchik <Shev4ik.den@gmail.com>
2014-05-31 09:31:35 +0400

    Skip global region when setting flags

--- src/WGExtender/commands/SetFlags.java
+++ src/WGExtender/commands/SetFlags.java
@@ -15,6 +15,7 @@ public class SetFlags {
 	public static <V> void setFlags(WorldGuardPlugin wg, Flag<V> flag, String args, World world) throws InvalidFlagFormat, ProtectionDatabaseException {
 		RegionManager rm = wg.getRegionManager(world);
 		for (ProtectedRegion region : rm.getRegions().values()) {
+			if (region.getId().equals("__global__")) {continue;}
 			region.setFlag(flag, flag.parseInput(wg, Bukkit.getConsoleSender(), args));
 		}
 		rm.save();
