5b91daf9a4c55063214da272354ee007c6742de6
Shevchik <Shev4ik.den@gmail.com>
2015-05-11 20:50:59 +0400

    Add commands to remove memeber/owner from all regions

--- src/wgextender/commands/Commands.java
+++ src/wgextender/commands/Commands.java
@@ -20,9 +20,11 @@ package wgextender.commands;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -36,11 +38,12 @@ import wgextender.Config;
 import wgextender.WGExtender;
 import wgextender.commands.RegionsInAreaSearch.NoSelectionException;
 import wgextender.features.claimcommand.AutoFlags;
-import wgextender.utils.ReflectionUtils;
 import wgextender.utils.StringUtils;
 import wgextender.utils.Transform;
 
 import com.sk89q.minecraft.util.commands.CommandException;
+import com.sk89q.worldguard.bukkit.RegionContainer;
+import com.sk89q.worldguard.domains.DefaultDomain;
 import com.sk89q.worldguard.protection.flags.BooleanFlag;
 import com.sk89q.worldguard.protection.flags.DefaultFlag;
 import com.sk89q.worldguard.protection.flags.EnumFlag;
@@ -48,6 +51,7 @@ import com.sk89q.worldguard.protection.flags.Flag;
 import com.sk89q.worldguard.protection.flags.InvalidFlagFormat;
 import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.flags.StateFlag.State;
+import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.GlobalProtectedRegion;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
@@ -59,65 +63,111 @@ public class Commands implements CommandExecutor, TabCompleter {
 		this.config = config;
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public boolean onCommand(CommandSender sender, Command arg1, String label, String[] args) {
 		if (!canExecute(sender)) {
 			sender.sendMessage(ChatColor.RED+"Недостаточно прав");
 			return true;
 		}
-		if ((args.length == 1) && args[0].equalsIgnoreCase("help")) {
-			sender.sendMessage(ChatColor.BLUE + "wgex reload - перезагрузить конфиг");
-			sender.sendMessage(ChatColor.BLUE + "wgex search - ищет регионы в выделенной области");
-			sender.sendMessage(ChatColor.BLUE + "wgex setflag {world} {flag} {value}  - устанавливает флаг {flag} со значением {value} на все регионы в мире {world}");
-			return true;
-		} else if ((args.length == 1) && args[0].equalsIgnoreCase("reload")) {
-			config.loadConfig();
-			sender.sendMessage(ChatColor.BLUE + "Конфиг перезагружен");
-			return true;
-		} else if ((args.length == 1) && args[0].equalsIgnoreCase("search")) {
-			if (sender instanceof Player) {
-				try {
-					List<String> regions = RegionsInAreaSearch.getRegionsInPlayerSelection((Player) sender);
-					if (regions.isEmpty()) {
-						sender.sendMessage(ChatColor.BLUE + "Регионов пересекающихся с выделенной зоной не найдено");
-						return true;
-					} else {
-						sender.sendMessage(ChatColor.BLUE + "Найдены регионы пересекающиеся с выделенной зоной: "+ regions);
-						return true;
-					}
-				} catch (NoSelectionException e) {
-					sender.sendMessage(ChatColor.BLUE + "Сначала выделите зону поиска");
+		if (args.length >= 1) {
+			switch (args[0].toLowerCase()) {
+				case "help": {
+					sender.sendMessage(ChatColor.BLUE + "wgex reload - перезагрузить конфиг");
+					sender.sendMessage(ChatColor.BLUE + "wgex search - ищет регионы в выделенной области");
+					sender.sendMessage(ChatColor.BLUE + "wgex setflag {world} {flag} {value}  - устанавливает флаг {flag} со значением {value} на все регионы в мире {world}");
+					sender.sendMessage(ChatColor.BLUE + "wgex removeowner {name} - удаляет игрока из списков владельцев всех регионов");
+					sender.sendMessage(ChatColor.BLUE + "wgex removemember {name} - удаляет игрока из списков владельцев всех регионов");
 					return true;
 				}
-			}
-		} else if ((args.length >= 4) && args[0].equalsIgnoreCase("setflag")) {
-			World world = Bukkit.getWorld(args[1]);
-			if (world != null) {
-				Flag<?> flag = DefaultFlag.fuzzyMatchFlag(args[2]);
-				if (flag != null) {
-					try {
-						String value = StringUtils.join(Arrays.copyOfRange(args, 3, args.length), " ");
-						for (ProtectedRegion region : WGExtender.getInstance().getWorldGuard().getRegionManager(world).getRegions().values()) {
-							if (region instanceof GlobalProtectedRegion) {
-								continue;
+				case "reload": {
+					config.loadConfig();
+					sender.sendMessage(ChatColor.BLUE + "Конфиг перезагружен");
+					return true;
+				}
+				case "search": {
+					if (sender instanceof Player) {
+						try {
+							List<String> regions = RegionsInAreaSearch.getRegionsInPlayerSelection((Player) sender);
+							if (regions.isEmpty()) {
+								sender.sendMessage(ChatColor.BLUE + "Регионов пересекающихся с выделенной зоной не найдено");
+							} else {
+								sender.sendMessage(ChatColor.BLUE + "Найдены регионы пересекающиеся с выделенной зоной: "+ regions);
 							}
-							AutoFlags.setFlag(region, flag, value);
+						} catch (NoSelectionException e) {
+							sender.sendMessage(ChatColor.BLUE + "Сначала выделите зону поиска");
 						}
-						sender.sendMessage(ChatColor.BLUE + "Флаги установлены");
-						return true;
-					} catch (InvalidFlagFormat e) {
-						sender.sendMessage(ChatColor.BLUE + "Неправильное значение для флага "+flag.getName()+": "+e.getMessage());
-						return true;
-					} catch (CommandException e) {
-						sender.sendMessage(ChatColor.BLUE + "Неправильный формат флага "+flag.getName()+": "+e.getMessage());
 						return true;
 					}
+					return false;
 				}
-				sender.sendMessage(ChatColor.BLUE + "Флаг не найден");
-				return true;
-			} else {
-				sender.sendMessage(ChatColor.BLUE + "Мир не найден");
-				return true;
+				case "setflag": {
+					if (args.length < 4) {
+						return false;
+					}
+					World world = Bukkit.getWorld(args[1]);
+					if (world != null) {
+						Flag<?> flag = DefaultFlag.fuzzyMatchFlag(args[2]);
+						if (flag != null) {
+							try {
+								String value = StringUtils.join(Arrays.copyOfRange(args, 3, args.length), " ");
+								for (ProtectedRegion region : WGExtender.getInstance().getWorldGuard().getRegionManager(world).getRegions().values()) {
+									if (region instanceof GlobalProtectedRegion) {
+										continue;
+									}
+									AutoFlags.setFlag(region, flag, value);
+								}
+								sender.sendMessage(ChatColor.BLUE + "Флаги установлены");
+							} catch (InvalidFlagFormat e) {
+								sender.sendMessage(ChatColor.BLUE + "Неправильное значение для флага "+flag.getName()+": "+e.getMessage());
+							} catch (CommandException e) {
+								sender.sendMessage(ChatColor.BLUE + "Неправильный формат флага "+flag.getName()+": "+e.getMessage());
+							}
+						}
+						sender.sendMessage(ChatColor.BLUE + "Флаг не найден");
+					} else {
+						sender.sendMessage(ChatColor.BLUE + "Мир не найден");
+					}
+					return true;
+				}
+				case "removeowner": {
+					if (args.length != 2) {
+						return false;
+					}
+					OfflinePlayer oplayer = Bukkit.getOfflinePlayer(args[1]);
+					String name = oplayer.getName();
+					UUID uuid = oplayer.getUniqueId();
+					RegionContainer container = WGExtender.getInstance().getWorldGuard().getRegionContainer();
+					for (RegionManager manager : container.getLoaded()) {
+						for (ProtectedRegion region : manager.getRegions().values()) {
+							DefaultDomain owners = region.getOwners();
+							owners.removePlayer(uuid);
+							owners.removePlayer(name.toLowerCase());
+							region.setOwners(owners);
+						}
+					}
+					sender.sendMessage(ChatColor.BLUE + "Игрок удалён из списков владельцев всех приватов");
+					return true;
+				}
+				case "removemember": {
+					if (args.length != 2) {
+						return false;
+					}
+					OfflinePlayer oplayer = Bukkit.getOfflinePlayer(args[1]);
+					String name = oplayer.getName();
+					UUID uuid = oplayer.getUniqueId();
+					RegionContainer container = WGExtender.getInstance().getWorldGuard().getRegionContainer();
+					for (RegionManager manager : container.getLoaded()) {
+						for (ProtectedRegion region : manager.getRegions().values()) {
+							DefaultDomain owners = region.getMembers();
+							owners.removePlayer(uuid);
+							owners.removePlayer(name.toLowerCase());
+							region.setMembers(owners);
+						}
+					}
+					sender.sendMessage(ChatColor.BLUE + "Игрок удалён из списков владельцев всех приватов");
+					return true;
+				}
 			}
 		}
 		return false;
