a751f999337b6f525f7e3df7760d8057ee035e38
Shevchik <Shev4ik.den@gmail.com>
2017-03-19 01:24:40 +0300

    Recalculate other damage

--- src/wgextender/features/custom/OldPVPFlagsHandler.java
+++ src/wgextender/features/custom/OldPVPFlagsHandler.java
@@ -1,7 +1,10 @@
 package wgextender.features.custom;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
 import org.bukkit.attribute.Attribute;
@@ -12,19 +15,25 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageModifier;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import com.google.common.base.Function;
 
 import wgextender.WGExtender;
 import wgextender.features.flags.OldPVPAttackSpeedFlag;
 import wgextender.features.flags.OldPVPNoShieldBlockFlag;
+import wgextender.utils.ReflectionUtils;
 import wgextender.utils.WGRegionUtils;
 
 public class OldPVPFlagsHandler implements Listener {
 
 	private final HashMap<UUID, Double> oldValues = new HashMap<>();
+	private Field functionsField;
 
-	public void start() {
+	public void start() throws NoSuchFieldException, SecurityException {
+		functionsField = ReflectionUtils.getField(EntityDamageEvent.class, "modifierFunctions");
 		Bukkit.getPluginManager().registerEvents(this, WGExtender.getInstance());
 		Bukkit.getScheduler().scheduleSyncRepeatingTask(WGExtender.getInstance(), new Runnable() {
 			@Override
@@ -44,12 +53,33 @@ public class OldPVPFlagsHandler implements Listener {
 		}, 0, 1);
 	}
 
+	@SuppressWarnings("unchecked")
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
 		Entity entity = event.getEntity();
 		if (entity instanceof Player) {
-			if (WGRegionUtils.isFlagTrue(entity.getLocation(), OldPVPNoShieldBlockFlag.getInstance())) {
-				event.setDamage(DamageModifier.BLOCKING, 0);
+			Player player = (Player) entity;
+			if (player.isBlocking() && WGRegionUtils.isFlagTrue(entity.getLocation(), OldPVPNoShieldBlockFlag.getInstance())) {
+				try {
+					Map<DamageModifier, Function<Double, Double>> func = (Map<DamageModifier, Function<Double, Double>>) functionsField.get(event);
+					double damage = event.getDamage() + event.getDamage(DamageModifier.HARD_HAT);
+					//reset blocking modifier
+					event.setDamage(DamageModifier.BLOCKING, 0);
+					//recalculate other modifiers
+					double armorModifier = func.get(DamageModifier.ARMOR).apply(damage);
+					event.setDamage(DamageModifier.ARMOR, armorModifier);
+					damage += armorModifier;
+					double resModifier = func.get(DamageModifier.RESISTANCE).apply(damage);
+					event.setDamage(DamageModifier.RESISTANCE, resModifier);
+					damage += resModifier;
+					double magicModifier = func.get(DamageModifier.MAGIC).apply(damage);
+					event.setDamage(DamageModifier.MAGIC, magicModifier);
+					damage += magicModifier;
+					double absorbtionModifier = func.get(DamageModifier.ABSORPTION).apply(damage);
+					event.setDamage(DamageModifier.ABSORPTION, absorbtionModifier);
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					WGExtender.getInstance().getLogger().log(Level.SEVERE, "Unable to recalculate blocking damage", e);
+				}
 			}
 		}
 	}
--- src/wgextender/utils/ReflectionUtils.java
+++ src/wgextender/utils/ReflectionUtils.java
@@ -18,12 +18,17 @@
 package wgextender.utils;
 
 import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Field;
 
 public class ReflectionUtils {
 
 	@SuppressWarnings("unchecked")
 	public static <T> T getField(Object obj, String fieldName) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
-		return (T) setAccessible(obj.getClass().getDeclaredField(fieldName)).get(obj);
+		return (T) getField(obj.getClass(), fieldName).get(obj);
+	}
+
+	public static Field getField(Class<?> clazz, String fieldName) throws NoSuchFieldException, SecurityException {
+		return setAccessible(clazz.getDeclaredField(fieldName));
 	}
 
 	public static <T extends AccessibleObject> T setAccessible(T object) {
